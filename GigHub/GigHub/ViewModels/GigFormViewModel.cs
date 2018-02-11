205e0ab1e37189219d2d091a4af0d907adb0e4f5
merisahakyan <sahakyan_m@bk.ru>
2018-02-11 20:51:14 +0400

    form with bootstrap

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date  { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
